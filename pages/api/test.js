647abdfe68ca8e29b3ad64707b491a0cbe8d6042
Lukáš Mareš <81.maryno@gmail.com>
2025-09-30 02:33:59 +0200

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,1 +1,6 @@
-
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    return res.status(200).json({ message: "API funguje" });
+  }
+  return res.status(405).json({ error: "Only GET allowed" });
+}
